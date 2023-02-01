a7cf2d5627523deaa93301d5f6b9fb1d8632f46c
Manikumar <kumarmani862003@gmail.com>
2023-02-01 07:27:20 +0530

    first commit

---
+++ TerminalStartup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+from datetime import datetime
+from datetime import date
+import subprocess as sp
+import  psutil,os,json
+
+battery = psutil.sensors_battery()
+os.system("journalctl -b --user-unit pulseaudio -g \"Battery Level\" -r -n 1 -o json > info.json")
+f = open("info.json")
+data= json.load(f)
+blue_con=sp.getoutput("hcitool con").split()
+blue_stat="Not connected"
+print("\tSystem  info\t\t\t\tBluetooth info ")
+print("\t============\t\t\t\t===============")
+
+print("Battery percentage: ",int(battery.percent),"%",end="\t\t",sep="")
+#check the bluetooth Devices connection status
+if(len(blue_con)!=1):
+    print("\t",data['MESSAGE'],end="")
+    if("8C:64:A2:6D:25:B3" in blue_con):
+        blue_stat="Mani's oneplus"
+    else:
+        blue_stat="unknow device"
+print("\nCharging state: ","Charging" if battery.power_plugged==True else "discharging",end="")
+print("\t\t Bluetooth Device: ",blue_stat)
+hrs=int(datetime.now().strftime("%H"))%12
+print("\n \t\tTime: ",hrs,datetime.now().strftime(":%M:%S"),sep="",end="")
+print("\tDate: ",date.today())
+
+print("\t\t\twifi info")
+print("\t\t\t=========")
+print(sp.getoutput("nmcli dev stat"))
