71d0dd5f4270cb402a8fcbae96f68d4f12e5c13c
Manikumar-T <kumarmani862003@gmail.com>
2023-04-27 07:12:29 +0530

    update ' Fixtures.py' pair  almost done

---  Fixtures.py
+++  Fixtures.py
@@ -7,18 +7,18 @@ Teams=['Immortals',
 'Troubleshooters',
 'Strikers',
 'Vaporizers',
-'Vigilantes','mani']
+'Vigilantes']
 
 FixtureDict ={}
 
 grpah = pydot.Dot(graph_type='digraph')
-def genFixtures():
-    random.shuffle(Teams)
-    print(Teams)
-    for i in Teams:
-        grpah.add_node(pydot.Node(name=i))
-    grpah.write_png('output.png')
-genFixtures()
+# def genFixtures():
+#     random.shuffle(Teams)
+#     print(Teams)
+#     for i in Teams:
+#         grpah.add_node(pydot.Node(name=i))
+#     grpah.write_png('output.png')
+# genFixtures()
 
 
 # for i in range(0,len(Teams),2):
@@ -29,9 +29,11 @@ genFixtures()
 def pair(list):
     temp=[]
     for i in range(0,len(list),2):
+        str=''
         #print(list[i:i+2])
-        # for i in list[i:i+2]:
-        temp.append(list[i:i+2])
+        for i in list[i:i+2]:
+            str+=i+" vs "
+        temp.append(str.strip(' vs '))
     return temp
 
 firstpair = pair(Teams)
@@ -39,11 +41,11 @@ print('first pair:',firstpair,'length: ',len(firstpair))
 
 print()
 
-# secondpair = pair(firstpair)
-# print('second pair',secondpair,'length: ',len(secondpair))
+secondpair = pair(firstpair)
+print('second pair',secondpair,'length: ',len(secondpair))
 
-# print()
+print()
 
-# thirdpari = pair(secondpair)
-# print('third pair',thirdpari,'length: ',len(thirdpari))
+thirdpari = pair(secondpair)
+print('third pair',thirdpari,'length: ',len(thirdpari))
 
