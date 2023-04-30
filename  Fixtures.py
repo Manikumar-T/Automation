17402547f9589728bdf58460b7602af13919dc64
Manikumar-T <kumarmani862003@gmail.com>
2023-04-30 19:54:24 +0530

    update ' Fixtures.py' after generate the Fixture in cli

---  Fixtures.py
+++  Fixtures.py
@@ -7,45 +7,46 @@ Teams=['Immortals',
 'Troubleshooters',
 'Strikers',
 'Vaporizers',
-'Vigilantes']
+'Vigilantes','mani']
 
 FixtureDict ={}
-
-grpah = pydot.Dot(graph_type='digraph')
-# def genFixtures():
-#     random.shuffle(Teams)
-#     print(Teams)
-#     for i in Teams:
-#         grpah.add_node(pydot.Node(name=i))
-#     grpah.write_png('output.png')
-# genFixtures()
-
-
-# for i in range(0,len(Teams),2):
-#     print(i)
-#     print(Teams[i:i+2])
-
 
 def pair(list):
     temp=[]
     for i in range(0,len(list),2):
-        str=''
+        str1=''
         #print(list[i:i+2])
         for i in list[i:i+2]:
-            str+=i+" vs "
-        temp.append(str.strip(' vs '))
+
+            str1+=str(i).replace('vs','/')+" vs "
+        temp.append(str1.strip(' vs '))
     return temp
 
-firstpair = pair(Teams)
-print('first pair:',firstpair,'length: ',len(firstpair))
+def genFixtureDict():
+    dictTemp=Teams
+    value=2
+    FixtureDict[1]=Teams
+    while(len(dictTemp)!=1):
+        dictTemp = pair(dictTemp)
+        print('{} pair length: {}'.format(dictTemp,len(dictTemp)))
+        FixtureDict[value] = dictTemp
+        value+=1
 
-print()
+    print(FixtureDict)
 
-secondpair = pair(firstpair)
-print('second pair',secondpair,'length: ',len(secondpair))
+grpah = pydot.Dot(graph_type='digraph')
+def genFixtures():
+    
+    random.shuffle(Teams)
+    print(Teams)
+    genFixtureDict()
+    for i in FixtureDict.keys():
+        for j in FixtureDict[i]:
+            grpah.add_node(pydot.Node(name=j))
+  
+            
 
-print()
+    grpah.write_png('output.png')
+genFixtures()
 
-thirdpari = pair(secondpair)
-print('third pair',thirdpari,'length: ',len(thirdpari))
 
